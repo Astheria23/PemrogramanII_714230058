83c8b7bf9d9070953c9d016798857b2ea3dddbd6
Octa Toriq <octa.toriq@gmail.com>
2024-10-15 16:11:20 +0700

    Praktikum Pertemuan 02

---
+++ Pertemuan02/Praktikum/P2_1_714230058/P2_1_714230058/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_714230058
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukan Angka Pertama :");
+          double angkaPertama = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Masukan Angka Kedua :");
+          angkaPertama = (angkaPertama);
+          int angkaKedua =Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" ========= HASIL ========== ");
+            Console.WriteLine("Angka Pertama " + angkaPertama);
+            Console.WriteLine("Angka Kedua " + angkaKedua);
+            Console.WriteLine($" {angkaPertama} +  {angkaKedua} = {angkaPertama + angkaKedua}" );
+            Console.WriteLine($" {angkaPertama} -  {angkaKedua} = {angkaPertama - angkaKedua}");
+            Console.WriteLine($" {angkaPertama} *  {angkaKedua} = {angkaPertama * angkaKedua}");
+            Console.WriteLine($" {angkaPertama} /  {angkaKedua} = {angkaPertama / angkaKedua}");
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
